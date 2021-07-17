f0ea7f9fd53603011be1b734fdeff291d8ea7ab6
Rithika Hebbar <rithikax24@gmail.com>
2021-07-17 11:34:58 +0530

    Update AboutActivity.java

--- AboutActivity.java
+++ AboutActivity.java
@@ -1,1 +1,9 @@
-
+package com.example.newlyindependent; 
+import android.app.Activity; 
+import android.os.Bundle; 
+public class AboutActivity extends Activity{ 
+    protected void onCreate(Bundle savedInstanceState) { 
+        super.onCreate(savedInstanceState); 
+        setContentView(R.layout.activity_about); 
+    } 
+} 
