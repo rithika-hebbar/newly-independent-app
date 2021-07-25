8455a2c3774f7a3cc766900dfd3f8a497a2a802b
Rithika Hebbar <rithikax24@gmail.com>
2021-07-25 21:59:08 +0530

    Update BreakfastActivity.java

--- BreakfastActivity.java
+++ BreakfastActivity.java
@@ -1,1 +1,49 @@
+package com.example.newlyindependent; 
+import android.app.ActionBar; 
+import android.app.Activity; 
+import android.content.Intent; 
+import android.os.Bundle; 
+import android.view.Menu; 
+import android.view.MenuInflater; 
+import android.view.View; 
+import android.widget.*;
+public class BreakfastActivity extends Activity{ 
+String name; 
+Button ravadosa, aloosand; 
+protected void onCreate(Bundle savedInstanceState){ 
+super.onCreate(savedInstanceState); 
+setContentView(R.layout.activity_breakfast); 
+ActionBar ab = getActionBar(); 
+ab.setDisplayHomeAsUpEnabled(true); 
+Intent i = getIntent(); 
+name = i.getExtras().getString("name"); 
+ravadosa = (Button)findViewById(R.id.btnrec); 
+aloosand = (Button)findViewById(R.id.btndel); 
+ravadosa.setOnClickListener(new View.OnClickListener() { 
+@Override 
+public void onClick(View arg0) { 
+// TODO Auto-generated method stub 
+Intent i = new Intent(getApplicationContext(), 
+RavadosaActivity.class); 
+i.putExtra("name", name); 
+startActivity(i); 
+} 
+}); 
+aloosand.setOnClickListener(new View.OnClickListener() { 
+@Override 
+public void onClick(View arg0) { 
+// TODO Auto-generated method stub 
+Intent i = new Intent(getApplicationContext(), 
+SandwichActivity.class); 
+i.putExtra("name", name); 
+startActivity(i); 
+}
+}); 
+} 
+public boolean onCreateOptionsMenu(Menu menu){ 
+MenuInflater inflater = getMenuInflater(); 
+inflater.inflate(R.menu.options, menu); 
+return super.onCreateOptionsMenu(menu); 
+} 
+} 
 
