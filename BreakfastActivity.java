87dcc8218be7a151e6dde189bda495fb58008cd6
Rithika Hebbar <rithikax24@gmail.com>
2021-07-27 11:18:25 +0530

    Update BreakfastActivity.java

--- BreakfastActivity.java
+++ BreakfastActivity.java
@@ -8,39 +8,39 @@ import android.view.MenuInflater;
 import android.view.View; 
 import android.widget.*;
 
-    public class BreakfastActivity extends Activity{ 
-        String name; 
-        Button ravadosa, aloosand; 
+public class BreakfastActivity extends Activity{ 
+    String name; 
+    Button ravadosa, aloosand; 
         
-        protected void onCreate(Bundle savedInstanceState){ 
-            super.onCreate(savedInstanceState); 
-            setContentView(R.layout.activity_breakfast); 
-            ActionBar ab = getActionBar(); 
-            ab.setDisplayHomeAsUpEnabled(true); 
-            Intent i = getIntent(); 
-            name = i.getExtras().getString("name"); 
-            ravadosa = (Button)findViewById(R.id.btnrec); 
-            aloosand = (Button)findViewById(R.id.btndel);
+    protected void onCreate(Bundle savedInstanceState){ 
+        super.onCreate(savedInstanceState); 
+        setContentView(R.layout.activity_breakfast); 
+        ActionBar ab = getActionBar(); 
+        ab.setDisplayHomeAsUpEnabled(true); 
+        Intent i = getIntent(); 
+        name = i.getExtras().getString("name"); 
+        ravadosa = (Button)findViewById(R.id.btnrec); 
+        aloosand = (Button)findViewById(R.id.btndel);
             
-            ravadosa.setOnClickListener(new View.OnClickListener() { 
-                @Override 
-                public void onClick(View arg0) { 
-                    // TODO Auto-generated method stub 
-                    Intent i = new Intent(getApplicationContext(), RavadosaActivity.class); 
-                    i.putExtra("name", name); 
-                    startActivity(i); 
-                } 
-            }); 
+        ravadosa.setOnClickListener(new View.OnClickListener() { 
+            @Override 
+            public void onClick(View arg0) { 
+                // TODO Auto-generated method stub 
+                Intent i = new Intent(getApplicationContext(), RavadosaActivity.class); 
+                i.putExtra("name", name); 
+                startActivity(i); 
+            } 
+        }); 
             
-            aloosand.setOnClickListener(new View.OnClickListener() { 
-                @Override 
-                public void onClick(View arg0) { 
-                    // TODO Auto-generated method stub 
-                    Intent i = new Intent(getApplicationContext(), SandwichActivity.class); 
-                    i.putExtra("name", name); 
-                    startActivity(i); 
-                }
-            }); 
+        aloosand.setOnClickListener(new View.OnClickListener() { 
+            @Override 
+            public void onClick(View arg0) { 
+                // TODO Auto-generated method stub 
+                Intent i = new Intent(getApplicationContext(), SandwichActivity.class); 
+                i.putExtra("name", name); 
+                startActivity(i); 
+            }
+        }); 
     } 
 
     public boolean onCreateOptionsMenu(Menu menu){ 
