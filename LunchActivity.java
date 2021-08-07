06db5c11b1197d3e394f837c25aa843ad38d3b62
Rithika Hebbar <rithikax24@gmail.com>
2021-08-07 16:24:02 +0530

    Update LunchActivity.java

--- LunchActivity.java
+++ LunchActivity.java
@@ -7,43 +7,45 @@ import android.view.Menu;
 import android.view.MenuInflater; 
 import android.view.View; 
 import android.widget.*; 
-public class LunchActivity extends Activity{ 
-String name; 
-Button lemon, rasam; 
-protected void onCreate(Bundle savedInstanceState){ 
-super.onCreate(savedInstanceState); 
-setContentView(R.layout.activity_lunch); 
-ActionBar ab = getActionBar(); 
-ab.setDisplayHomeAsUpEnabled(true); 
-Intent i = getIntent(); 
-name = i.getExtras().getString("name"); 
-lemon = (Button)findViewById(R.id.btnrec); 
-rasam = (Button)findViewById(R.id.btndel); 
-lemon.setOnClickListener(new View.OnClickListener() { 
-@Override 
-public void onClick(View arg0) { 
-// TODO Auto-generated method stub 
-Intent i = new Intent(getApplicationContext(), 
-LemonActivity.class); 
-i.putExtra("name", name); 
-startActivity(i); 
-} 
-});
-rasam.setOnClickListener(new View.OnClickListener() { 
-@Override 
-public void onClick(View arg0) { 
-// TODO Auto-generated method stub 
-Intent i = new Intent(getApplicationContext(), 
-RasamActivity.class); 
-i.putExtra("name", name); 
-startActivity(i); 
-} 
-}); 
-} 
-public boolean onCreateOptionsMenu(Menu menu){ 
-MenuInflater inflater = getMenuInflater(); 
-inflater.inflate(R.menu.options, menu); 
-return super.onCreateOptionsMenu(menu); 
-} 
-}
 
+public class LunchActivity extends Activity{ 
+    String name; 
+    Button lemon, rasam; 
+    
+    protected void onCreate(Bundle savedInstanceState){ 
+        super.onCreate(savedInstanceState); 
+        setContentView(R.layout.activity_lunch); 
+        ActionBar ab = getActionBar(); 
+        ab.setDisplayHomeAsUpEnabled(true); 
+        Intent i = getIntent(); 
+        name = i.getExtras().getString("name"); 
+        lemon = (Button)findViewById(R.id.btnrec); 
+        rasam = (Button)findViewById(R.id.btndel); 
+        
+        lemon.setOnClickListener(new View.OnClickListener() { 
+            @Override 
+            public void onClick(View arg0) { 
+                // TODO Auto-generated method stub 
+                Intent i = new Intent(getApplicationContext(), LemonActivity.class); 
+                i.putExtra("name", name); 
+                startActivity(i); 
+            } 
+        });
+        
+        rasam.setOnClickListener(new View.OnClickListener() { 
+            @Override 
+            public void onClick(View arg0) { 
+                // TODO Auto-generated method stub 
+                Intent i = new Intent(getApplicationContext(), RasamActivity.class); 
+                i.putExtra("name", name); 
+                startActivity(i); 
+            } 
+        }); 
+    } 
+    
+    public boolean onCreateOptionsMenu(Menu menu){ 
+        MenuInflater inflater = getMenuInflater(); 
+        inflater.inflate(R.menu.options, menu); 
+        return super.onCreateOptionsMenu(menu); 
+    } 
+}
