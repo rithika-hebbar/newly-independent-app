b7f00a55b3b2488e6093989e50ffc7af1c04403d
Rithika Hebbar <rithikax24@gmail.com>
2021-11-09 12:24:12 +0530

    Update ChapatiActivity.java

--- ChapatiActivity.java
+++ ChapatiActivity.java
@@ -16,7 +16,7 @@ public class ChapatiActivity extends Activity{
     
     protected void onCreate(Bundle savedInstanceState){ 
         super.onCreate(savedInstanceState); 
-requestWindowFeature(Window.FEATURE_NO_TITLE); 
+        requestWindowFeature(Window.FEATURE_NO_TITLE); 
 setContentView(R.layout.activity_chapati); 
 Intent i = getIntent(); 
 name = i.getExtras().getString("name"); 
