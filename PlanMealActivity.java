8bd88987168fb2d022f536b1b11aa9ce6f4a7746
Rithika Hebbar <rithikax24@gmail.com>
2022-06-22 23:11:49 +0530

    Update PlanMealActivity.java

--- PlanMealActivity.java
+++ PlanMealActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.Window; 
 import android.widget.*; 
 import android.widget.AdapterView.OnItemSelectedListener;
+
 public class PlanMealActivity extends Activity{ 
 String name, item; 
 TextView itemtv; 
