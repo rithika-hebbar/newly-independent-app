244d5a6739d474f6760661321be9f71fdf1213d3
Rithika Hebbar <rithikax24@gmail.com>
2022-07-04 10:30:57 +0530

    Update PlanMealActivity.java

--- PlanMealActivity.java
+++ PlanMealActivity.java
@@ -27,8 +27,8 @@ public class PlanMealActivity extends Activity{
         item = i.getExtras().getString("item"); 
         dbname = name; 
         itemtv = (TextView)findViewById(R.id.tvdate); 
-itemtv.setText(item); 
-dbitem = item; 
+        itemtv.setText(item); 
+      dbitem = item; 
 save = (Button)findViewById(R.id.btnrec); 
 meal = (Spinner)findViewById(R.id.spinner1); 
 date = (DatePicker)findViewById(R.id.datePicker1); 
