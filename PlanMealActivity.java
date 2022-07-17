5487dd1aae14578134c878710059059ba460ac88
Rithika Hebbar <rithikax24@gmail.com>
2022-07-17 09:03:22 +0530

    Update PlanMealActivity.java

--- PlanMealActivity.java
+++ PlanMealActivity.java
@@ -38,8 +38,7 @@ public class PlanMealActivity extends Activity{
         
         meal.setOnItemSelectedListener(new OnItemSelectedListener(){ 
             @Override 
-            public void onItemSelected(AdapterView<?> arg0, View arg1,
-int arg2, long arg3) { 
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) { 
 // TODO Auto-generated method stub 
 int index = meal.getSelectedItemPosition(); 
 dbmeal = meals[index]; 
