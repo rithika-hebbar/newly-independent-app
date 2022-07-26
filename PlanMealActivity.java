78f5e15beaa6c750d0391ffc999114c67d98d791
Rithika Hebbar <rithikax24@gmail.com>
2022-07-27 00:01:33 +0530

    Update PlanMealActivity.java

--- PlanMealActivity.java
+++ PlanMealActivity.java
@@ -53,7 +53,7 @@ public class PlanMealActivity extends Activity{
         save.setOnClickListener(new View.OnClickListener() { 
             @Override 
             public void onClick(View arg0) { 
-// TODO Auto-generated method stub 
+                // TODO Auto-generated method stub 
 dbdate = getDate(); 
 Boolean ins = mydb.insertMeal(dbname, dbitem, dbmeal, dbdate); 
 if(ins){ 
