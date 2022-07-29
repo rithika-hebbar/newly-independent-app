8562003a72397a2d957895fc72c40cb83d01a9b6
Rithika Hebbar <rithikax24@gmail.com>
2022-07-29 20:17:50 +0530

    Update PlanMealActivity.java

--- PlanMealActivity.java
+++ PlanMealActivity.java
@@ -58,7 +58,7 @@ public class PlanMealActivity extends Activity{
                 Boolean ins = mydb.insertMeal(dbname, dbitem, dbmeal, dbdate); 
                 
                 if(ins){ 
-Toast.makeText(PlanMealActivity.this, "Added to 
+                    Toast.makeText(PlanMealActivity.this, "Added to 
 planner!", Toast.LENGTH_SHORT).show(); 
 Intent mpi = new Intent(getApplicationContext(), 
 HomeActivity.class); 
