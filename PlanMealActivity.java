ddccf0f7223b11f3351b2b78d221e01c409bb3cc
Rithika Hebbar <rithikax24@gmail.com>
2022-09-11 18:17:09 +0530

    Update PlanMealActivity.java

--- PlanMealActivity.java
+++ PlanMealActivity.java
@@ -72,7 +72,7 @@ public class PlanMealActivity extends Activity{
     public String getDate(){ 
         StringBuffer data = new StringBuffer(); 
         data.append((date.getMonth()+1)+"/");
-data.append(date.getDayOfMonth()+"/"); 
+        data.append(date.getDayOfMonth()+"/"); 
 data.append(date.getYear()); 
 return data.toString(); 
 } 
