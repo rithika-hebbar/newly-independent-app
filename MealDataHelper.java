8dc69458a550c784367feb85e1529e4faee5b044
Rithika Hebbar <rithikax24@gmail.com>
2022-09-18 19:35:56 +0530

    Update MealDataHelper.java

--- MealDataHelper.java
+++ MealDataHelper.java
@@ -20,7 +20,7 @@ public class MealDataHelper extends SQLiteOpenHelper {
     } 
 
     @Override 
-public void onUpgrade(SQLiteDatabase mydb, int arg1, int arg2) { 
+    public void onUpgrade(SQLiteDatabase mydb, int arg1, int arg2) { 
 // TODO Auto-generated method stub 
 mydb.execSQL("DROP TABLE IF EXISTS mealplan"); 
 onCreate(mydb); 
