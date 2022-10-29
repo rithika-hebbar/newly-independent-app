760f5a69c20f9c5e8fb993f2d83ccac167979c7f
Rithika Hebbar <rithikax24@gmail.com>
2022-10-30 00:07:11 +0530

    Update MealPlanActivity.java

--- MealPlanActivity.java
+++ MealPlanActivity.java
@@ -17,7 +17,8 @@ public class MealPlanActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState){ 
         super.onCreate(savedInstanceState); 
         requestWindowFeature(Window.FEATURE_NO_TITLE); 
-        setContentView(R.layout.activity_mealplan); mydb = new MealDataHelper(this); 
+        setContentView(R.layout.activity_mealplan); 
+        mydb = new MealDataHelper(this); 
 Intent i = getIntent(); 
 name = i.getExtras().getString("name"); 
 populateListView(); 
