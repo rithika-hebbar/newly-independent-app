91956d4d375e16904286620ec1f2f690834725d9
Rithika Hebbar <rithikax24@gmail.com>
2022-11-06 12:42:53 +0530

    Update MealPlanActivity.java

--- MealPlanActivity.java
+++ MealPlanActivity.java
@@ -30,8 +30,7 @@ public class MealPlanActivity extends Activity{
         startManagingCursor(result); 
         String[] fields = new String[]{"_id", "item","meal","date"};
         int[] viewid = new int[]{R.id.tvnone,R.id.tvitem, R.id.tvmeal, R.id.tvdate}; 
-        SimpleCursorAdapter ca = new SimpleCursorAdapter(this, R.layout.layout_listview, result, fields, viewid); 
-ListView lv = (ListView)findViewById(R.id.listView1); 
+        SimpleCursorAdapter ca = new SimpleCursorAdapter(this, R.layout.layout_listview, result, fields, viewid); ListView lv = (ListView)findViewById(R.id.listView1); 
 lv.setAdapter(ca); 
 } 
 public void listViewClick(){ 
