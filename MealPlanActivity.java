15cfcb81901161bac39499971f2653c66ce6eeb1
Rithika Hebbar <rithikax24@gmail.com>
2022-11-08 17:40:23 +0530

    Update MealPlanActivity.java

--- MealPlanActivity.java
+++ MealPlanActivity.java
@@ -37,7 +37,7 @@ public class MealPlanActivity extends Activity{
     
     public void listViewClick(){ 
         ListView lv = (ListView)findViewById(R.id.listView1); 
-lv.setOnItemClickListener(new AdapterView.OnItemClickListener() { 
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() { 
 @Override 
 public void onItemClick(AdapterView<?> parent, View viewclick, int pos, long id) { 
 // TODO Auto-generated method stub 
