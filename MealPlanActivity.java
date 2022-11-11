c19c572185786f19be4916ff1d018036317c8379
Rithika Hebbar <rithikax24@gmail.com>
2022-11-11 12:53:11 +0530

    Update MealPlanActivity.java

--- MealPlanActivity.java
+++ MealPlanActivity.java
@@ -54,8 +54,7 @@ public class MealPlanActivity extends Activity{
                     lvi.putExtra("id", String.valueOf(iddb)); 
                     lvi.putExtra("item", item); 
                     lvi.putExtra("meal", meal); 
-                    lvi.putExtra("date", date); 
-lvi.putExtra("name", name); 
+                    lvi.putExtra("date", date); lvi.putExtra("name", name); 
 startActivity(lvi); 
 } 
 } 
